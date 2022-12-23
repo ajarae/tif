0ba5f09d05b54ffe88ef7812fd862273f67e8311
ajarae <121247846+ajarae@users.noreply.github.com>
2022-12-23 17:00:31 -0500

    Update 03 Procesos.py

--- pages/03 Procesos.py
+++ pages/03 Procesos.py
@@ -167,9 +167,9 @@ st.markdown("""
 components.html(
     """
     
-<center><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglowCz3JVIsPlVcVXo4H3Vle292h8M9nA3LYpzAoqKNkZyEZIOHjUIV13M61gto1DsdxnWaweljEMzGoQXz5TGXHpp_9Q8TINLQRtb9jq-irVmy4CmzMm5GtrMZmXXmKwDWX2boxRzBe5GYguX_SN_8bEncQAb0m7sZaySai42wl1GoqNQoasvsileWw/s320/1.png" width="800" height="700"></center><br/>
+<center><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiX0XpBzhYHmgI-HmYvXg9DKxZ0u4N52TkiOoxdU63PBnQIxfOuMZX3pDhgGebScCtFllLo_AYNoNm9e8eQLWgQYSDb3BsoAbeHzdhrQ9iAc7Md8Fm90rx-of4hAUwL9in5ZDFF5Leg4TrwFQbzWCtYFrmdkQdK9D39hNJlupTc0-KzYSaUC6ZacQA4qw/w945-h600-p-k-no-nu/4.png" width="945" height="600"></center><br/>
 
-    """,height="800",
+    """,height="1000",
 )
 #fig, ax = plt.subplots()
 #sns.heatmap(m_corr_d_pandas, vmin=-1, vmax=1, cmap = 'YlOrRd')
