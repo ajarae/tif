bf83952e15ca5438ff1d1fcbb3dbbd8bcf898d42
ajarae <121247846+ajarae@users.noreply.github.com>
2022-12-23 16:53:27 -0500

    Update 03 Procesos.py

--- pages/03 Procesos.py
+++ pages/03 Procesos.py
@@ -160,13 +160,16 @@ st.code(code, language='python')
 #plt.title('MAPA DE CALOR DE LA CORRELACIÓN', fontsize = 10))
 #plt.colorbar())
 #plt.show()
+st.markdown("""
+![1](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglowCz3JVIsPlVcVXo4H3Vle292h8M9nA3LYpzAoqKNkZyEZIOHjUIV13M61gto1DsdxnWaweljEMzGoQXz5TGXHpp_9Q8TINLQRtb9jq-irVmy4CmzMm5GtrMZmXXmKwDWX2boxRzBe5GYguX_SN_8bEncQAb0m7sZaySai42wl1GoqNQoasvsileWw/s320/1.png)
+""")
 
 components.html(
     """
     
 <center><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglowCz3JVIsPlVcVXo4H3Vle292h8M9nA3LYpzAoqKNkZyEZIOHjUIV13M61gto1DsdxnWaweljEMzGoQXz5TGXHpp_9Q8TINLQRtb9jq-irVmy4CmzMm5GtrMZmXXmKwDWX2boxRzBe5GYguX_SN_8bEncQAb0m7sZaySai42wl1GoqNQoasvsileWw/s320/1.png" width="600" height="500"></center><br/>
 
-    """height="600",
+    """,height="600",
 )
 #fig, ax = plt.subplots()
 #sns.heatmap(m_corr_d_pandas, vmin=-1, vmax=1, cmap = 'YlOrRd')
