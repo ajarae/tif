c65c80bfa5ba52abbeeaba8beea4da2db2574f7d
ajarae <121247846+ajarae@users.noreply.github.com>
2022-12-23 17:20:29 -0500

    Update 03 Procesos.py

--- pages/03 Procesos.py
+++ pages/03 Procesos.py
@@ -160,14 +160,10 @@ st.code(code, language='python')
 #plt.title('MAPA DE CALOR DE LA CORRELACIÓN', fontsize = 10))
 #plt.colorbar())
 #plt.show()
-#st.markdown("""
-#![1](https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglowCz3JVIsPlVcVXo4H3Vle292h8M9nA3LYpzAoqKNkZyEZIOHjUIV13M61gto1DsdxnWaweljEMzGoQXz5TGXHpp_9Q8TINLQRtb9jq-irVmy4CmzMm5GtrMZmXXmKwDWX2boxRzBe5GYguX_SN_8bEncQAb0m7sZaySai42wl1GoqNQoasvsileWw/s1460/1.png)
-#""")
+st.markdown("""
 
-components.html(
-    """
 <center><img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEglowCz3JVIsPlVcVXo4H3Vle292h8M9nA3LYpzAoqKNkZyEZIOHjUIV13M61gto1DsdxnWaweljEMzGoQXz5TGXHpp_9Q8TINLQRtb9jq-irVmy4CmzMm5GtrMZmXXmKwDWX2boxRzBe5GYguX_SN_8bEncQAb0m7sZaySai42wl1GoqNQoasvsileWw/s1460/1.png" width="500" height="600"> </center>
-    """,height="600",
+    """
 )
 
 
